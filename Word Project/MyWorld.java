467b4e70e75210bfaf4b04f74d8c5f877a44d9ff
Shawn348973371 <348973371@gapps.yrdsb.ca>
2021-11-01 10:35:30 -0400

    Moved changes to method

--- Word Project/MyWorld.java
+++ Word Project/MyWorld.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.util.ArrayList;
 /**
  * Write a description of class MyWorld here.
  * 
@@ -17,22 +17,22 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
-<<<<<<< Updated upstream
-=======
+       
+        
+        
+        
+    }
+    
+    public String generateString(){    
+        //Gets random number, then finds that index on the list of words on the url in reader.
         
         ArrayList<String> myList = new ArrayList<String>();
         try{
                 Reader.readInto(myList);
         } catch(Exception e) {
         }
-        
-        
-    }
-    
-    public String generateString(ArrayList<String> myList){    
-        //Gets random number, then finds that index on the list of words on the url in reader.
+        //the loop above should be in the constructor of whatever world the game is on
         int n = Greenfoot.getRandomNumber(10000);
         return myList.get(n);
->>>>>>> Stashed changes
     }
 }
