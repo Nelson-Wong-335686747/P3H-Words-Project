1cd7ded004c618c17d22b38b289116a73a35fd1d
Shawn348973371 <348973371@gapps.yrdsb.ca>
2021-11-01 10:27:25 -0400

    Implemented Reader in 'MyWorld'

--- Word Project/MyWorld.java
+++ Word Project/MyWorld.java
@@ -17,5 +17,22 @@ public class MyWorld extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(600, 400, 1); 
+<<<<<<< Updated upstream
+=======
+        
+        ArrayList<String> myList = new ArrayList<String>();
+        try{
+                Reader.readInto(myList);
+        } catch(Exception e) {
+        }
+        
+        
+    }
+    
+    public String generateString(ArrayList<String> myList){    
+        //Gets random number, then finds that index on the list of words on the url in reader.
+        int n = Greenfoot.getRandomNumber(10000);
+        return myList.get(n);
+>>>>>>> Stashed changes
     }
 }
