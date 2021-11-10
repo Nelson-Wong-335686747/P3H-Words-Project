1cec3e3cd952204a8260e6827a0dfbe0ca0ff94b
Nelson-Wong-335686747 <335686747@gapps.yrdsb.ca>
2021-11-09 22:48:13 -0500

    Game flows way smoother

--- GameWorld.java
+++ GameWorld.java
@@ -112,21 +112,21 @@ public class GameWorld extends World
         {
             //spawns words every set amount of time, depending on the current level
             
-            if((spawnTimer > 180 - 15*level) && level < 9)  
+            if((spawnTimer >= 180 - 15*level) && level <= 9)  
             {
                 addWord();
             }
-            else if(spawnTimer > 30 && level > 10)
+            else if(spawnTimer >= 30 && level > 10)
             {
                 addWord(); 
             }
             
             //moves all the words (aside from user input) every set amount of time, depending on the current level
-            if(moveTimer>120 -(15*level) && level < 6)
+            if(moveTimer >= 120 - (15*level) && level <= 6)
             {
                 moveWords(30);
             }
-            else if(level > 6 && moveTimer>25) //If the moveTimer decreased any further, words would move way too fast for it to be playable
+            else if(level > 6 && moveTimer >= 25) //If the moveTimer decreased any further, words would move way too fast for it to be playable
             {
                 moveWords(30);
             }
